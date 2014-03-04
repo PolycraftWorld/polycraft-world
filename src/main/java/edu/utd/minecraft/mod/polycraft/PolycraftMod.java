f9a6134c641c0db13224e413f66a9ff5f37af07d
jim-mcandrew <jim@mcandrewland.com>
2014-03-04 17:08:08 -0600

    Oil

--- src/main/java/edu/utd/minecraft/mod/polycraft/PolycraftMod.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/PolycraftMod.java
@@ -1,12 +1,23 @@
 package edu.utd.minecraft.mod.polycraft;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameData;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import edu.utd.minecraft.mod.polycraft.proxy.CommonProxy;
 
 @Mod(modid = PolycraftMod.MODID, version = PolycraftMod.VERSION)
 public class PolycraftMod
@@ -14,12 +25,24 @@ public class PolycraftMod
     public static final String MODID = "polycraft";
     public static final String VERSION = "1.0";
     
+    @SidedProxy(clientSide="edu.utd.minecraft.mod.polycraft.proxy.CombinedClientProxy", serverSide="edu.utd.minecraft.mod.polycraft.proxy.DedicatedServerProxy")
+    public static CommonProxy proxy;
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit();
+    }
+    
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-    	//Dirt to diamonds!
-        ItemStack dirtStack = new ItemStack(Blocks.dirt);
-        ItemStack diamondsStack = new ItemStack(Items.diamond, 64);
-        GameRegistry.addShapelessRecipe(diamondsStack, dirtStack);
+    	proxy.init();
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    	proxy.postInit();
     }
 }
