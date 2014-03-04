a89dd76a94f8f596e03fd8e94a2869c2ab7a361c
jim-mcandrew <jim@mcandrewland.com>
2014-03-04 11:44:56 -0600

    Rename to Polycraft

--- src/main/java/edu/utd/minecraft/mod/polycraft/PolycraftMod.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/PolycraftMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package edu.utd.minecraft.mod.polycraft;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -8,10 +8,10 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = PolycraftMod.MODID, version = PolycraftMod.VERSION)
+public class PolycraftMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "polycraft";
     public static final String VERSION = "1.0";
     
     @EventHandler
