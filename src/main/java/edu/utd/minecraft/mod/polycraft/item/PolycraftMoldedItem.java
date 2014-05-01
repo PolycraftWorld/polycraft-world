4557f2bc7a092fee02edeba10b442662ed7d6210
jim-mcandrew <jim@mcandrewland.com>
2014-05-01 16:14:37 -0500

    Abstracted molded item

---
+++ src/main/java/edu/utd/minecraft/mod/polycraft/item/PolycraftMoldedItem.java
@@ -0,0 +1,7 @@
+package edu.utd.minecraft.mod.polycraft.item;
+
+import edu.utd.minecraft.mod.polycraft.config.MoldedItem;
+
+public interface PolycraftMoldedItem {
+	public MoldedItem getMoldedItem();
+}
