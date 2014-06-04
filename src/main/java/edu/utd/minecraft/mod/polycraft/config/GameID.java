5f817e94511bbe8e5d42079b868a1c48919e3075
jim-mcandrew <jim@mcandrewland.com>
2014-06-04 15:14:40 -0500

    Added element vessels

---
+++ src/main/java/edu/utd/minecraft/mod/polycraft/config/GameID.java
@@ -0,0 +1,35 @@
+package edu.utd.minecraft.mod.polycraft.config;
+
+public enum GameID {
+	MoldedRunningShoes("E"),
+	MoldScubaFins("G"),
+	MoldScubaMask("H"),
+	MoldLifePreserver("J"),
+	InventoryTreeTap("3h"),
+	InventoryMachiningMill("3i"),
+	InventoryExtruder("3j"),
+	InventoryInjectionMolder("3k"),
+	InventoryDistillationColumn("3E"),
+	InventorySteamCracker("3D"),
+	InventoryFueledLamp("3V"),
+	InventorySpotlight("41"),
+	CustomBucketOil("3m"),
+	CustomFlameThrower("3n"),
+	CustomFlashlight("3o"),
+	CustomJetPack("3p"),
+	CustomParachute("5a"),
+	CustomPhaseShifter("3r"),
+	CustomScubaTank("3x"),
+	CustomKevlarVest("5b"),
+	InternalOil("1D");
+
+	private final String id;
+
+	private GameID(final String id) {
+		this.id = id;
+	}
+
+	public boolean matches(final GameIdentifiedConfig config) {
+		return config.gameID.equals(id);
+	}
+}
