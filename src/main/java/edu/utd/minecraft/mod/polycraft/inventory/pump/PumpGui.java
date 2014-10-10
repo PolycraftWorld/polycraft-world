b7f825a2c486e3db6ba9e678d6426ee44081ba0c
jim-mcandrew <jim@mcandrewland.com>
2014-10-09 21:10:51 -0500

    Added some more info

--- src/main/java/edu/utd/minecraft/mod/polycraft/inventory/pump/PumpGui.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/inventory/pump/PumpGui.java
@@ -27,31 +27,33 @@ public class PumpGui extends PolycraftInventoryGui<PumpInventory> {
 		super.drawGuiContainerForegroundLayer(p_146979_1_, p_146979_2_);
 		final int fuelTicksRemaining = inventory.getState(PumpState.FuelTicksRemaining);
 		final int fuelTicksRemainingEpochs = inventory.getState(PumpState.FuelTicksRemainingEpochs);
+		final int color = flowNetwork.isValid() ? 0x129E00 : 0x9E0300;
+		int offset = 30;
 		if (fuelTicksRemaining > 0 || fuelTicksRemainingEpochs > 0) {
-			final int color = flowNetwork.isValid() ? 0x129E00 : 0x9E0300;
 			final Fuel fuel = Fuel.getFuel(inventory.getState(PumpState.FuelIndex));
-			this.fontRendererObj.drawString(fuelName + fuel.name, 8, 40, color);
-			this.fontRendererObj.drawString(fuelIntensity + inventory.getState(PumpState.FuelHeatIntensity), 8, 50, color);
+			this.fontRendererObj.drawString(fuelName + fuel.name, 8, offset += 10, color);
+			this.fontRendererObj.drawString(fuelIntensity + inventory.getState(PumpState.FuelHeatIntensity), 8, offset += 10, color);
 			this.fontRendererObj.drawString(String.format(fuelRemaining, ((double) 
 					(fuelTicksRemaining + fuelTicksRemainingEpochs*maxTicksPerEpoch) / 
 					(double) (inventory.getState(PumpState.FuelTicksTotal) + 
-							(inventory.getState(PumpState.FuelTicksTotalEpochs)*maxTicksPerEpoch))) * 100, inventory.getState(PumpState.FlowTicksRemaining)), 8, 60, color);
-			if (flowNetwork.isValid())
-			{
-				this.fontRendererObj.drawString(String.format(sourceDistance, flowNetwork.source.distanceFromPump), 8, 70, color);
-				this.fontRendererObj.drawString(String.format(defaultTargetDistance, flowNetwork.defaultTarget.distanceFromPump), 8, 80, color);
-				if (flowNetwork.regulatedTargets.size() > 0)
-					this.fontRendererObj.drawString(String.format(regulatedTargets, flowNetwork.regulatedTargets.size()), 8, 90, color);
-			}
+							(inventory.getState(PumpState.FuelTicksTotalEpochs)*maxTicksPerEpoch))) * 100, inventory.getState(PumpState.FlowTicksRemaining)), 8, offset += 10, color);
+		}
+		else {
+			this.fontRendererObj.drawString("Add fuel to start pumping!", 8, offset += 10, color);
+		}
+		if (flowNetwork.isValid()) {
+			this.fontRendererObj.drawString(String.format(sourceDistance, flowNetwork.source.distanceFromPump), 8, offset += 10, color);
+			this.fontRendererObj.drawString(String.format(defaultTargetDistance, flowNetwork.defaultTarget.distanceFromPump), 8, offset += 10, color);
+			if (flowNetwork.regulatedTargets.size() > 0)
+				this.fontRendererObj.drawString(String.format(regulatedTargets, flowNetwork.regulatedTargets.size()), 8, offset += 10, color);
+		}
+		else {
+			if (flowNetwork.source == null)
+				this.fontRendererObj.drawString("Invalid source", 8, offset += 10, color);
+			else if (flowNetwork.defaultTarget == null)
+				this.fontRendererObj.drawString("Invalid default target", 8, offset += 10, color);
 			else
-			{
-				if (flowNetwork.source == null)
-					this.fontRendererObj.drawString("Invalid source", 8, 70, color);
-				else if (flowNetwork.defaultTarget == null)
-					this.fontRendererObj.drawString("Invalid default target", 8, 70, color);
-				else
-					this.fontRendererObj.drawString("Invalid regulated target(s)", 8, 70, color);
-			}
+				this.fontRendererObj.drawString("Invalid regulated target(s)", 8, offset += 10, color);
 		}
 	}
 }
