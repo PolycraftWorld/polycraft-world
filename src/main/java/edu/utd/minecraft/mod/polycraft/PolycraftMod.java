d3d6c7543f32926761d4459d7769ba634c0b07f8
Walter Voit (Windows) <walter.voit@utdallas.edu>
2015-05-21 09:24:53 -0500

    Update to 1.2.5

--- src/main/java/edu/utd/minecraft/mod/polycraft/PolycraftMod.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/PolycraftMod.java
@@ -49,8 +49,8 @@ import edu.utd.minecraft.mod.polycraft.worldgen.BiomeGenOilOcean;
 @Mod(modid = PolycraftMod.MODID, version = PolycraftMod.VERSION)
 public class PolycraftMod {
 	public static final String MODID = "polycraft";
-	public static final String VERSION = "1.2.4";
-	private static final int[] VERSION_NUMERIC = new int[] { 1, 2, 4 };
+	public static final String VERSION = "1.2.5";
+	private static final int[] VERSION_NUMERIC = new int[] { 1, 2, 5 };
 	public static final Logger logger = LogManager.getFormatterLogger(MODID);
 	public static final NumberFormat numFormat = NumberFormat.getInstance();
 
