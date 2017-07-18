8af87aebd29af565b8ee13a866b45e6d91889ee0
dkreth <dylankreth@gmail.com>
2017-07-18 11:11:32 -0500

    draw correct tabs on ComputerGui (active vs inactive vs hovering-shaded)

--- src/main/java/edu/utd/minecraft/mod/polycraft/inventory/computer/ComputerGui.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/inventory/computer/ComputerGui.java
@@ -16,29 +16,49 @@ import net.minecraft.util.ResourceLocation;
 @SideOnly(Side.CLIENT)
 public class ComputerGui extends /*GuiContainer */ PolycraftInventoryGui<ComputerInventory>{
 
-	public static final ResourceLocation computerBaseGui = new ResourceLocation(PolycraftMod.MODID,"textures/gui/new_computer_with_active_tabs_connected.png");
+	public static final ResourceLocation computerBaseGui = new ResourceLocation(PolycraftMod.MODID,"textures/gui/newer_computer_with_active_tabs_separated_darkened.png");
 	private ComputerInventory computer;
+	private final GuiRectangle[] tabs;
+	private GuiRectangle activeTab;
 	
-	public ComputerGui(ComputerInventory inventory, InventoryPlayer playerInventory) {
+	public ComputerGui(ComputerInventory inventory, InventoryPlayer playerInventory, int xSize, int ySize) {
 //	public ComputerGui(InventoryPlayer invPlayer, ComputerInventory computer) {
 	
 		
-		super(inventory, playerInventory, 379, false);
+		super(inventory, playerInventory);
+		this.xSize=xSize;
+		this.ySize=ySize;
 		//super(new ComputerContainer( invPlayer, computer));
-		this.xSize=342;
-		this.ySize=330;
-		this.computer=computer;
+//		this.xSize=342;
+//		this.ySize=330;
+//		this.computer=computer;
+		tabs = new GuiRectangle[4];
+		tabs[0] = new GuiRectangle(336,23,52,60);
+		tabs[1] = new GuiRectangle(336,84,52,60);
+		tabs[2] = new GuiRectangle(336,145,52,60);
+		tabs[3] = new GuiRectangle(336,206,52,60);
+		activeTab = tabs[0];
 	}
 	
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTickTime, int mouseX, int mouseY){
 		glColor4f(1,1,1,1);
 		Minecraft.getMinecraft().getTextureManager().bindTexture(computerBaseGui);
-		drawTexturedModalRect(0,0,0,0,xSize/2,ySize/2);
-		/*Logger logger =  LogManager.getFormatterLogger("polycraft");
-		logger.info("ok");
-		logger.info(guiLeft);
-		logger.info(guiTop);*/
+		drawTexturedModalRect(guiLeft,guiTop,0,0,xSize,ySize);
+		int srcX,srcY;
+		for (int i = 0; i<tabs.length; i++){
+			int side = 2;
+//			if (i>2)
+//				side = 2;
+			GuiRectangle rectangle = tabs[i];
+			srcX = 342/2;
+			srcY = 145/2;
+			if ((rectangle.inRect(this, mouseX, mouseY, side) && tabs[i] != activeTab) || false)
+				srcY = 84/2;
+			if (tabs[i] == activeTab)
+				srcY = 23/2;
+			rectangle.draw(this, srcX, srcY, side);
+		}
 	}
 	
 	@Override
@@ -47,5 +67,19 @@ public class ComputerGui extends /*GuiContainer */ PolycraftInventoryGui<Compute
 		super.initGui();
 	}
 
+	public int getLeft(){
+		return guiLeft;
+	}
 	
+	public int getTop(){
+		return guiTop;
+	}
+	
+	public int getXSize(){
+		return xSize;
+	}
+	
+	public int getYSize(){
+		return ySize;
+	}
 }
