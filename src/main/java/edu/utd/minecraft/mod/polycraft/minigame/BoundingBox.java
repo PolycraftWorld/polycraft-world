a541b80aae0b83aae64e20549657443f9e8d2773
Chris Wang <chriswang444@gmail.com>
2018-09-14 11:53:10 -0500

    Finished prototype bounding box.

--- src/main/java/edu/utd/minecraft/mod/polycraft/minigame/BoundingBox.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/minigame/BoundingBox.java
@@ -5,37 +5,178 @@ import java.awt.Color;
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 
+/**
+ * Simple collision box for minigames.
+ * 
+ * @author Chris
+ */
 public class BoundingBox {
 
-	private boolean yBound = false;
-	private Color color;
-	private float lineWidth = 4;
-	private double x1, z1, x2, z2;
-	private float y1 = 0, y2 = 256;
-	private boolean rendering = true;
+	private boolean yBound = false; // If the bounding box checks an entity's height for collisions.
+	private Color color = Color.GREEN; // The color of the rendered bounding box.
+	private float lineWidth = 4; // The width of rendered bounding box lines.
+	private double x1, z1, x2, z2; // The (x, z) coordinates of the northwest and southeast corners.
+	private float y1 = 0, y2 = 256; // The lowest and highest points of the bounding box.
+	private boolean rendering = true; // True if the bounding box is visible to clients.
 
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;x1&gt;, &lt;z1&gt;, &lt;x2&gt;, &lt;z2&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with two nonadjacent corners specified as (x1, z1) and
+	 * (x2, z2) in that order.
+	 * 
+	 * @param x1
+	 * @param z1
+	 * @param x2
+	 * @param z2
+	 */
 	public BoundingBox(double x1, double z1, double x2, double z2) {
 		setBounds(x1, z1, x2, z2);
-		setColor(Color.GREEN);
 	}
 
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;x1&gt;, &lt;z1&gt;, &lt;x2&gt;, &lt;z2&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with two nonadjacent corners specified as (x1, z1) and
+	 * (x2, z2) in that order alongside a color.
+	 * 
+	 * @param x1
+	 * @param z1
+	 * @param x2
+	 * @param z2
+	 * @param color
+	 */
 	public BoundingBox(double x1, double z1, double x2, double z2, Color color) {
 		setBounds(x1, z1, x2, z2);
 		setColor(color);
 	}
 
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;xc&gt;, &lt;zc&gt;, &lt;radius&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with a center specified as (xc, zc) and a radius.
+	 * 
+	 * @param xc
+	 * @param zc
+	 * @param radius
+	 */
 	public BoundingBox(double xc, double zc, double radius) {
 		setBounds(xc, zc, radius);
-		setColor(Color.GREEN);
 	}
 
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;xc&gt;, &lt;zc&gt;, &lt;radius&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with a center specified as (xc, zc), a radius, and a
+	 * color.
+	 * 
+	 * @param xc
+	 * @param zc
+	 * @param radius
+	 * @param color
+	 */
 	public BoundingBox(double xc, double zc, double radius, Color color) {
 		setBounds(xc, zc, radius);
 		setColor(color);
 	}
 
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;x1&gt;, &lt;z1&gt;, &lt;x2&gt;, &lt;z2&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with two nonadjacent corners specified as (x1, z1) and
+	 * (x2, z2) in that order, a floor (y1), and a roof (y2).
+	 * 
+	 * @param x1
+	 * @param z1
+	 * @param x2
+	 * @param z2
+	 * @param y1
+	 * @param y2
+	 */
+	public BoundingBox(double x1, double z1, double x2, double z2, float y1, float y2) {
+		setBounds(x1, z1, x2, z2, y1, y2);
+	}
+
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;x1&gt;, &lt;z1&gt;, &lt;x2&gt;, &lt;z2&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with two nonadjacent corners specified as (x1, z1) and
+	 * (x2, z2) in that order, a floor (y1), a roof (y2), and a color.
+	 * 
+	 * @param x1
+	 * @param z1
+	 * @param x2
+	 * @param z2
+	 * @param y1
+	 * @param y2
+	 * @param color
+	 */
+	public BoundingBox(double x1, double z1, double x2, double z2, float y1, float y2, Color color) {
+		setBounds(x1, z1, x2, z2, y1, y2);
+		setColor(color);
+	}
+
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;xc&gt;, &lt;zc&gt;, &lt;radius&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with a center specified as (xc, zc), a radius, a floor
+	 * (y1), and a roof (y2).
+	 * 
+	 * @param xc
+	 * @param zc
+	 * @param radius
+	 * @param y1
+	 * @param y2
+	 */
+	public BoundingBox(double xc, double zc, double radius, float y1, float y2) {
+		setBounds(xc, zc, radius, y1, y2);
+	}
+
+	/**
+	 * <pre>
+	 * BoundingBox(&lt;xc&gt;, &lt;zc&gt;, &lt;radius&gt;, [y1], [y2], [{@link Color}])
+	 * </pre>
+	 * 
+	 * Set up a bounding box with a center specified as (xc, zc), a radius, a floor
+	 * (y1), a roof (y2), and a color.
+	 * 
+	 * @param xc
+	 * @param zc
+	 * @param radius
+	 * @param y1
+	 * @param y2
+	 * @param color
+	 */
+	public BoundingBox(double xc, double zc, double radius, float y1, float y2, Color color) {
+		setBounds(xc, zc, radius, y1, y2);
+		setColor(color);
+	}
+
+	/**
+	 * Reshapes the bounding box with two nonadjacent corners specified as (x1, z1)
+	 * and (x2, z2) in that order.
+	 * 
+	 * @param x1
+	 * @param z1
+	 * @param x2
+	 * @param z2
+	 * @return this object
+	 */
 	public BoundingBox setBounds(double x1, double z1, double x2, double z2) {
 		this.x1 = Math.min(x1, x2);
 		this.z1 = Math.min(z1, z2);
@@ -45,6 +186,14 @@ public class BoundingBox {
 		return this;
 	}
 
+	/**
+	 * Reshapes the bounding box with a center specified as (xc, zc) and a radius.
+	 * 
+	 * @param xc
+	 * @param zc
+	 * @param radius
+	 * @return this object
+	 */
 	public BoundingBox setBounds(double xc, double zc, double radius) {
 		this.x1 = xc - radius;
 		this.z1 = zc - radius;
@@ -54,11 +203,59 @@ public class BoundingBox {
 		return this;
 	}
 
+	/**
+	 * 
+	 * Reshapes the bounding box with two nonadjacent corners specified as (x1, z1)
+	 * and (x2, z2) in that order, a floor (y1), and a roof (y2).
+	 * 
+	 * @param x1
+	 * @param z1
+	 * @param x2
+	 * @param z2
+	 * @param y1
+	 * @param y2
+	 * @return this object
+	 */
+	public BoundingBox setBounds(double x1, double z1, double x2, double z2, float y1, float y2) {
+		return setBounds(x1, z1, x2, z2).setYBounds(y1, y2);
+	}
+
+	/**
+	 * Reshapes the bounding box with a center specified as (xc, zc), a radius, a
+	 * floor (y1), and a roof (y2).
+	 * 
+	 * @param xc
+	 * @param zc
+	 * @param radius
+	 * @param y1
+	 * @param y2
+	 * @return this object
+	 */
+	public BoundingBox setBounds(double xc, double zc, double radius, float y1, float y2) {
+		return setBounds(xc, zc, radius).setYBounds(y1, y2);
+	}
+
+	/**
+	 * Set to true if the collision of the bounding box should also check for
+	 * height.
+	 * 
+	 * @param yBound
+	 * @return this object
+	 */
 	public BoundingBox setYBound(boolean yBound) {
 		this.yBound = yBound;
 		return this;
 	}
 
+	/**
+	 * Resizes the height of the bounding box based on the specified floor (y1) and
+	 * roof (y2). <br>
+	 * This also automatically sets if the box is yBound to true.
+	 * 
+	 * @param y1
+	 * @param y2
+	 * @return this object
+	 */
 	public BoundingBox setYBounds(float y1, float y2) {
 		this.y1 = Math.min(y1, y2);
 		this.y2 = Math.max(y1, y2);
@@ -67,34 +264,85 @@ public class BoundingBox {
 		return this;
 	}
 
+	/**
+	 * True if the specified entity is touching the bounding box in any capacity.
+	 * 
+	 * @param entity
+	 * @return boolean
+	 */
 	public boolean isInBox(Entity entity) {
 		if (yBound && (entity.boundingBox.maxY < y1 || entity.boundingBox.minY > y2))
 			return false;
 		return entity.posX >= x1 && entity.posX <= x2 && entity.posZ >= z1 && entity.posZ <= z2;
 	}
 
+	/**
+	 * Get the {@link Color} of the bounding box.
+	 * 
+	 * @return Color
+	 */
 	public Color getColor() {
 		return color;
 	}
 
+	/**
+	 * Set the {@link Color} of the bounding box if visible.
+	 * 
+	 * @param color
+	 * @return this object
+	 */
 	public BoundingBox setColor(Color color) {
 		this.color = color;
 		return this;
 	}
 
+	/**
+	 * The width of the rendered line.
+	 * 
+	 * @return float
+	 */
+	public float getLineWidth() {
+		return lineWidth;
+	}
+
+	/**
+	 * Set the width of the lines of the bounding box.
+	 * 
+	 * @param lineWidth
+	 * @return this object
+	 */
+	public BoundingBox setLineWidth(float lineWidth) {
+		this.lineWidth = lineWidth;
+		return this;
+	}
+
+	/**
+	 * True if the bounding box is visible on clients.
+	 * 
+	 * @return boolean
+	 */
 	public boolean isRendering() {
 		return rendering;
 	}
 
+	/**
+	 * Set to true if the bounding box should be visible on clients.
+	 * 
+	 * @param rendering
+	 * @return this object
+	 */
 	public BoundingBox setRendering(boolean rendering) {
 		this.rendering = rendering;
 		return this;
 	}
 
+	// Rendering variables.
 	private int xRange, yRange, zRange, hRange;
 	private float xSpacing, ySpacing, zSpacing;
 
 	/**
+	 * Determines parameters for drawing the bounding box ahead of time.
+	 * 
 	 * I'm sorry.
 	 */
 	private void calculateAESTHETICS() {
@@ -108,90 +356,70 @@ public class BoundingBox {
 		hRange /= 2;
 	}
 
+	/**
+	 * This should be called by the minigame manager on the client's side when
+	 * rendering.
+	 */
 	public void render(Entity entity) {
 		if (entity.worldObj.isRemote && rendering) {
 			GL11.glDisable(GL11.GL_TEXTURE_2D);
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			GL11.glDisable(GL11.GL_LIGHTING);
-			// GL11.glDisable(GL11.GL_CULL_FACE);
 			GL11.glLineWidth(lineWidth);
 			GL11.glBegin(GL11.GL_LINES);
+			GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F,
+					color.getAlpha() / 255F); // Set color to specified color.
 
-			if (isInBox(entity))
-				GL11.glColor4f(1 - color.getRed() / 255F, 1 - color.getGreen() / 255F, 1 - color.getBlue() / 255F,
-						color.getAlpha() / 255F);
+			float offset = (entity.ticksExisted % 20) / 20F; // For render animation.
 
-			else
-				GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F,
-						color.getAlpha() / 255F);
-
-			float offset = (entity.ticksExisted % 20) / 20F;
-			float offset2 = 1 - offset;
-
-			if (yBound) {
+			if (yBound) { // Top and bottom of bounding box render.
 				for (int i = 0; i < hRange; i++) {
 					float offset3 = i + offset;
-					float offset4 = i + offset2;
-					GL11.glVertex3d(x1 + xSpacing * offset4, y2, z1 + zSpacing * offset4);
-					GL11.glVertex3d(x2 - xSpacing * offset4, y2, z1 + zSpacing * offset4);
-					GL11.glVertex3d(x2 - xSpacing * offset4, y2, z1 + zSpacing * offset4);
-					GL11.glVertex3d(x2 - xSpacing * offset4, y2, z2 - zSpacing * offset4);
-					GL11.glVertex3d(x2 - xSpacing * offset4, y2, z2 - zSpacing * offset4);
-					GL11.glVertex3d(x1 + xSpacing * offset4, y2, z2 - zSpacing * offset4);
-					GL11.glVertex3d(x1 + xSpacing * offset4, y2, z2 - zSpacing * offset4);
-					GL11.glVertex3d(x1 + xSpacing * offset4, y2, z1 + zSpacing * offset4);
+					float offset4 = i + 1 - offset;
+					double x1o3 = x1 + xSpacing * offset3;
+					double x1o4 = x1 + xSpacing * offset4;
+					double x2o3 = x2 - xSpacing * offset3;
+					double x2o4 = x2 - xSpacing * offset4;
+					double z1o3 = z1 + zSpacing * offset3;
+					double z1o4 = z1 + zSpacing * offset4;
+					double z2o3 = z2 - zSpacing * offset3;
+					double z2o4 = z2 - zSpacing * offset4;
+					GL11.glVertex3d(x1o4, y2, z1o4);
+					GL11.glVertex3d(x2o4, y2, z1o4);
+					GL11.glVertex3d(x2o4, y2, z1o4);
+					GL11.glVertex3d(x2o4, y2, z2o4);
+					GL11.glVertex3d(x2o4, y2, z2o4);
+					GL11.glVertex3d(x1o4, y2, z2o4);
+					GL11.glVertex3d(x1o4, y2, z2o4);
+					GL11.glVertex3d(x1o4, y2, z1o4);
 
-					GL11.glVertex3d(x1 + xSpacing * offset3, y1, z1 + zSpacing * offset3);
-					GL11.glVertex3d(x2 - xSpacing * offset3, y1, z1 + zSpacing * offset3);
-					GL11.glVertex3d(x2 - xSpacing * offset3, y1, z1 + zSpacing * offset3);
-					GL11.glVertex3d(x2 - xSpacing * offset3, y1, z2 - zSpacing * offset3);
-					GL11.glVertex3d(x2 - xSpacing * offset3, y1, z2 - zSpacing * offset3);
-					GL11.glVertex3d(x1 + xSpacing * offset3, y1, z2 - zSpacing * offset3);
-					GL11.glVertex3d(x1 + xSpacing * offset3, y1, z2 - zSpacing * offset3);
-					GL11.glVertex3d(x1 + xSpacing * offset3, y1, z1 + zSpacing * offset3);
+					GL11.glVertex3d(x1o3, y1, z1o3);
+					GL11.glVertex3d(x2o3, y1, z1o3);
+					GL11.glVertex3d(x2o3, y1, z1o3);
+					GL11.glVertex3d(x2o3, y1, z2o3);
+					GL11.glVertex3d(x2o3, y1, z2o3);
+					GL11.glVertex3d(x1o3, y1, z2o3);
+					GL11.glVertex3d(x1o3, y1, z2o3);
+					GL11.glVertex3d(x1o3, y1, z1o3);
 				}
 			}
 
+			// Sides of bounding box render.
 			for (int i = 0; i < yRange; i++) {
-				GL11.glVertex3d(x1, y2 - ySpacing * (i + offset), z1);
-				GL11.glVertex3d(x2, y2 - ySpacing * (i + offset), z1);
+				float height = y2 - ySpacing * (i + offset);
+				GL11.glVertex3d(x1, height, z1);
+				GL11.glVertex3d(x2, height, z1);
 
-				GL11.glVertex3d(x2, y2 - ySpacing * (i + offset), z1);
-				GL11.glVertex3d(x2, y2 - ySpacing * (i + offset), z2);
+				GL11.glVertex3d(x2, height, z1);
+				GL11.glVertex3d(x2, height, z2);
 
-				GL11.glVertex3d(x2, y2 - ySpacing * (i + offset), z2);
-				GL11.glVertex3d(x1, y2 - ySpacing * (i + offset), z2);
+				GL11.glVertex3d(x2, height, z2);
+				GL11.glVertex3d(x1, height, z2);
 
-				GL11.glVertex3d(x1, y2 - ySpacing * (i + offset), z2);
-				GL11.glVertex3d(x1, y2 - ySpacing * (i + offset), z1);
+				GL11.glVertex3d(x1, height, z2);
+				GL11.glVertex3d(x1, height, z1);
 			}
-
-			// GL11.glColor4f(1, 0, 0, 0.25F);
-			// if (yBound) {
-			// GL11.glVertex3d(x1, y1, z1); // A
-			// GL11.glVertex3d(x1, y1, z2); // B
-			// }
-			// GL11.glVertex3d(x2, y1, z1); // C
-			// GL11.glVertex3d(x2, y1, z2); // D
-			// GL11.glColor4f(0, 1, 0, 0.25F);
-			// GL11.glVertex3d(x2, y2, z2); // E
-			// GL11.glColor4f(0, 0, 1, 0.25F);
-			// GL11.glVertex3d(x1, y1, z2); // F (B)
-			// GL11.glVertex3d(x1, y2, z2); // G
-			// GL11.glColor4f(0, 1, 0, 0.25F);
-			// GL11.glVertex3d(x1, y1, z1); // H (A)
-			// GL11.glVertex3d(x1, y2, z1); // I
-			// GL11.glColor4f(0, 0, 1, 0.25F);
-			// GL11.glVertex3d(x2, y1, z1); // J (C)
-			// GL11.glVertex3d(x2, y2, z1); // K
-			// GL11.glColor4f(0, 1, 0, 0.25F);
-			// GL11.glVertex3d(x2, y2, z2); // L (E)
-			// if (yBound) {
-			// GL11.glColor4f(1, 0, 0, 0.25F);
-			// GL11.glVertex3d(x1, y2, z1); // M (I)
-			// GL11.glVertex3d(x1, y2, z2); // N (G)
-			// }
 
 			GL11.glEnd();
 			GL11.glEnable(GL11.GL_CULL_FACE);
