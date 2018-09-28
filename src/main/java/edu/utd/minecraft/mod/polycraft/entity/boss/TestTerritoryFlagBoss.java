85dc493992dca5750ce8c5f6a4dba39eaaa9008e
Chris Wang <chriswang444@gmail.com>
2018-09-28 13:27:06 -0500

    Finished prototype attack setup

--- src/main/java/edu/utd/minecraft/mod/polycraft/entity/boss/TestTerritoryFlagBoss.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/entity/boss/TestTerritoryFlagBoss.java
@@ -15,6 +15,7 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIArrowAttack;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.boss.BossStatus;
 import net.minecraft.entity.boss.IBossDisplayData;
 import net.minecraft.entity.monster.EntityMob;
@@ -22,6 +23,8 @@ import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
@@ -77,8 +80,6 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 		this.summonAttacks = new LinkedList<Attack>();
 		attacker = new EntityAIArrowAttack(this, 1.0D, 1, 1, DETECTION_RANGE);
 		this.tasks.addTask(4, attacker);
-		// this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class,
-		// 8.0F));
 		// Note: boolean val determines if to call for help. Should be false.
 		this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, false));
 		// Note: boolean val determines if LoS is required.
@@ -90,7 +91,7 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 		super.applyEntityAttributes();
 		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(DETECTION_RANGE);
 		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.0);
-		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(1000D);
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(500D);
 		// maxHurtResistantTime = 10;
 	}
 
@@ -99,34 +100,47 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 		return true;
 	}
 
+	@Override
+	protected boolean isMovementBlocked() {
+		return true;
+	}
+
+	@Override
+	protected boolean canDespawn() {
+		return false;
+	}
+
 	// Initial State
 	private boolean active = false;
 
 	// Arrow Attacks
-	private static final int ARROW_COOLDOWN = 20;
+	private static final int ARROW_MAX_COOLDOWN = 40;
+	private static final int ARROW_MIN_COOLDOWN = 10;
 	private static final int ARROWS = 8;
-	private int arrowCooldown = ARROW_COOLDOWN;
+	private int arrowCooldown = ARROW_MAX_COOLDOWN;
 	private int arrows = ARROWS;
 
 	// Lightning Attacks
 	private static final int LIGHTNING_ATTACK_RADIUS = 3;
 	private static final int LIGHTNING_MAX_COOLDOWN = 200;
 	private static final int LIGHTNING_MIN_COOLDOWN = 40;
-	private static final int LIGHTNING_RECHARGE = 60;
+	private static final int LIGHTNING_RECHARGE = 20;
 
 	private int lightningBolts = 0;
-	private int lightningCooldown = LIGHTNING_MIN_COOLDOWN;
+	private int lightningCooldown = LIGHTNING_MAX_COOLDOWN;
 	private boolean lightningDischarge = false;
 	private int lightningRecharge = LIGHTNING_RECHARGE;
 
 	// Siphon Attacks
 	private static final int SIPHON_ATTACK_RADIUS = 1;
+	private static final int SIPHON_CHARGE = 200;
+	private int siphonCooldown = 0;
 
 	// Spawner Attacks
 	private static final int SUMMONING_RADIUS = 4;
-	private static final int SUMMON_MAX_COOLDOWN = 20;// 2400;
-	private static final int SUMMON_MIN_COOLDOWN = 20;// 600;
-	private int summonCooldown = SUMMON_MIN_COOLDOWN;
+	private static final int SUMMON_MAX_COOLDOWN = 2400;
+	private static final int SUMMON_MIN_COOLDOWN = 1200;
+	private int summonCooldown = 0;
 
 	public float scaleToHealth(int min, int max) {
 		float fraction = getHealth() / getMaxHealth();
@@ -142,10 +156,15 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 				new AttackWarning(victim.posX, victim.posZ, LIGHTNING_ATTACK_RADIUS, h, LIGHTNING_ATTACK_RADIUS * 2, 60)
 						.setColor(Color.YELLOW));
 		lightningAttacks.add(
-				new Attack(victim.posX, h, victim.posZ, LIGHTNING_ATTACK_RADIUS, LIGHTNING_ATTACK_RADIUS * 2, 20, 60));
+				new Attack(victim.posX, h, victim.posZ, LIGHTNING_ATTACK_RADIUS, LIGHTNING_ATTACK_RADIUS * 2, 10, 60));
 		lightningBolts--;
 		lightningCooldown = (int) scaleToHealth(LIGHTNING_MIN_COOLDOWN, LIGHTNING_MAX_COOLDOWN);
 	}
+
+	private EntityMob siphonTarget = null;
+	private double siphonX;
+	private double siphonY;
+	private double siphonZ;
 
 	@Override
 	public void onLivingUpdate() {
@@ -153,33 +172,37 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 			updateAITasks();
 		World world = this.worldObj;
 		if (active && world instanceof WorldServer) {
-			if (world.isDaytime())
+			if (world.isDaytime()) // Force night time.
 				world.setWorldTime(world.getWorldTime() + 1000);
 			WorldServer worldServer = (WorldServer) world;
 			List victims = world.selectEntitiesWithinAABB(EntityPlayer.class,
 					this.boundingBox.expand(DETECTION_RANGE, DETECTION_RANGE, DETECTION_RANGE), attackEntitySelector);
 			int numPlayers = victims.size();
 			// Reset accidental target.
-			if (this.getAITarget() == this)
+			if (this.getAITarget() instanceof TestTerritoryFlagBoss)
 				this.setTarget(null);
 			// Siphoning health from other mobs.
 			else if ((this.getAITarget() instanceof EntityMob)) {
-				this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0D);
-				this.getAITarget().hurtResistantTime = 0;
-				if (this.ticksExisted % 20 == 0) {
-					EntityMob victim = (EntityMob) this.getAITarget();
-					float percent = this.getAITarget().getMaxHealth() / 10F;
+				EntityMob victim = (EntityMob) this.getAITarget();
+				if (siphonCooldown == 0 && !victim.isDead) {
+					siphonTarget = victim;
+					siphonX = victim.posX;
+					siphonY = victim.posY;
+					siphonZ = victim.posZ;
+					victim.hurtResistantTime = 0;
+					float death = this.getAITarget().getMaxHealth();
 					int y = worldServer.getPrecipitationHeight((int) Math.round(victim.posX),
 							(int) Math.round(victim.posZ));
 					double height = victim.height + Math.abs(victim.posY - y);
-					siphonAttacks
-							.add(new Attack(victim.posX, y, victim.posZ, SIPHON_ATTACK_RADIUS, height, percent, 20));
-					AttackWarning.sendPackets(
-							new AttackWarning(victim.posX, victim.posZ, SIPHON_ATTACK_RADIUS, y, height, 20));
+					siphonAttacks.add(new Attack(victim.posX, y, victim.posZ, SIPHON_ATTACK_RADIUS, height, death,
+							SIPHON_CHARGE));
+					AttackWarning.sendPackets(new AttackWarning(victim.posX, victim.posZ, SIPHON_ATTACK_RADIUS, y,
+							height, SIPHON_CHARGE));
+					siphonCooldown = SIPHON_CHARGE + 1;
 				}
+				this.setRevengeTarget(null);
 			} else { // Attacks on players.
-				if (lightningBolts >= victims.size()
-						&& lightningCooldown < scaleToHealth(LIGHTNING_MIN_COOLDOWN, LIGHTNING_MAX_COOLDOWN) / 2) {
+				if (lightningBolts >= victims.size() && lightningCooldown < 2) {
 					for (int i = 0; i < victims.size(); i++) {
 						EntityPlayer victim = (EntityPlayer) victims.get(i);
 						if (!victim.capabilities.isCreativeMode) {
@@ -207,6 +230,7 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 				}
 			}
 
+			// Execute LightningAttacks
 			while (!lightningAttacks.isEmpty() && lightningAttacks.getFirst().warnTicks == 0) {
 				Attack attack = lightningAttacks.removeFirst();
 				worldServer.addWeatherEffect(
@@ -216,6 +240,12 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 				attack.warnTicks--;
 
 			// Execute Siphon Attacks
+			if (siphonTarget != null) {
+				if (siphonTarget.isDead)
+					siphonTarget = null;
+				else
+					siphonTarget.setPosition(siphonX, siphonY, siphonZ);
+			}
 			while (!siphonAttacks.isEmpty() && siphonAttacks.getFirst().warnTicks == 0) {
 				Attack attack = siphonAttacks.removeFirst();
 				List list = this.worldObj.getEntitiesWithinAABBExcludingEntity(this,
@@ -225,15 +255,21 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 				for (int i = 0; i < list.size(); i++) {
 					Entity entity = (Entity) list.get(i);
 					if (entity instanceof EntityLivingBase) {
+						System.out.println(attack.d);
 						((EntityLivingBase) entity).attackEntityFrom(siphon, attack.d);
 						this.heal(attack.d);
 					}
+				}
+				if (siphonTarget != null && !siphonTarget.isDead) {
+					this.heal(siphonTarget.getHealth());
+					siphonTarget.setHealth(0);
+					siphonTarget = null;
 				}
 			}
 			for (Attack attack : siphonAttacks)
 				attack.warnTicks--;
 
-			// Execute Spawning Attacks
+			// Execute Summoning Attacks
 			while (!summonAttacks.isEmpty() && summonAttacks.getFirst().warnTicks == 0) {
 				Attack attack = summonAttacks.removeFirst();
 				int maxSpawn = this.rand.nextInt(Math.max((numPlayers + 1) / 2, 1)) + 1;
@@ -241,8 +277,10 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 					EntityMob toSpawn;
 					if (this.rand.nextBoolean())
 						toSpawn = new EntityZombie(world);
-					else
+					else {
 						toSpawn = new EntitySkeleton(world);
+						toSpawn.setCurrentItemOrArmor(0, new ItemStack(Items.bow));
+					}
 					toSpawn.forceSpawn = true;
 					toSpawn.setPosition(attack.x + (this.rand.nextDouble() - 0.5) * attack.r * 2, attack.y + 1,
 							attack.z + (this.rand.nextDouble() - 0.5) * attack.r * 2);
@@ -260,9 +298,9 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 			// Arrow Cooldown
 			if (arrowCooldown > 0)
 				arrowCooldown--;
-			if (arrowCooldown == 0) {
+			if (arrowCooldown == 0 && arrows < numPlayers * 2) {
 				arrows++;
-				arrowCooldown = ARROW_COOLDOWN;
+				arrowCooldown = (int) scaleToHealth(ARROW_MIN_COOLDOWN, ARROW_MAX_COOLDOWN);
 			}
 
 			// Lightning Cooldown
@@ -275,6 +313,10 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 				lightningRecharge = LIGHTNING_RECHARGE;
 			}
 
+			// Siphon Cooldown
+			if (siphonCooldown > 0)
+				siphonCooldown--;
+
 			// Summon Cooldown
 			if (summonCooldown > 0)
 				summonCooldown--;
@@ -282,17 +324,19 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 		} else if (world.isRemote) {
 			BossStatus.setBossStatus(this, true);
 		}
-
 	}
 
 	@Override
-	protected boolean isMovementBlocked() {
-		return true;
-	}
-
-	@Override
-	protected boolean canDespawn() {
-		return false;
+	public void onDeath(DamageSource source) {
+		super.onDeath(source);
+		World world = this.worldObj;
+		world.playBroadcastSound(1013, (int) this.posX, (int) this.posY, (int) this.posZ, 0);
+		if (!world.isRemote) {
+			List victims = world.selectEntitiesWithinAABB(EntityMob.class,
+					this.boundingBox.expand(DETECTION_RANGE, DETECTION_RANGE, DETECTION_RANGE), null);
+			for (int i = 0; i < victims.size(); i++)
+				((EntityMob) victims.get(i)).setHealth(0);
+		}
 	}
 
 	@Override
@@ -303,8 +347,14 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 				active = true;
 				this.worldObj.playBroadcastSound(1013, (int) this.posX, (int) this.posY, (int) this.posZ, 0);
 				// Wither scary noise lol
-			} else
-				lightningCooldown /= 2;
+			} else {
+				if (arrowCooldown / 2 >= ARROW_MAX_COOLDOWN / 2)
+					arrowCooldown /= 2;
+				if (lightningCooldown / 2 >= LIGHTNING_MIN_COOLDOWN)
+					lightningCooldown /= 2;
+				if (summonCooldown / 2 >= SUMMON_MAX_COOLDOWN / 2)
+					summonCooldown /= 2;
+			}
 		}
 		return result;
 	}
@@ -329,7 +379,7 @@ public class TestTerritoryFlagBoss extends EntityMob implements IBossDisplayData
 				this.playSound("random.bow", 1.0F, 1.0F / (this.getRNG().nextFloat() * 0.4F + 0.8F));
 				this.worldObj.spawnEntityInWorld(entityarrow);
 				arrows--;
-				arrowCooldown = ARROW_COOLDOWN;
+				arrowCooldown = (int) scaleToHealth(ARROW_MIN_COOLDOWN, ARROW_MAX_COOLDOWN);
 			}
 		}
 	}
