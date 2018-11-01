cefa7f30139b37b6e0a2c42e8389a8e0f32774ce
StephenGss <Stephengs94@gmail.com>
2018-11-01 12:47:26 -0500

    added exit command as it's own command
    Previously was in the challenge command

---
+++ src/main/java/edu/utd/minecraft/mod/polycraft/commands/CommandExit.java
@@ -0,0 +1,101 @@
+package edu.utd.minecraft.mod.polycraft.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.utd.minecraft.mod.polycraft.experiment.Experiment;
+import edu.utd.minecraft.mod.polycraft.experiment.ExperimentCTB;
+import edu.utd.minecraft.mod.polycraft.experiment.ExperimentManager;
+import edu.utd.minecraft.mod.polycraft.privateproperty.Enforcer;
+import edu.utd.minecraft.mod.polycraft.privateproperty.PrivateProperty;
+import edu.utd.minecraft.mod.polycraft.privateproperty.ServerEnforcer;
+import edu.utd.minecraft.mod.polycraft.privateproperty.SuperChunk;
+import edu.utd.minecraft.mod.polycraft.privateproperty.PrivateProperty.Chunk;
+import edu.utd.minecraft.mod.polycraft.worldgen.PolycraftTeleporter;
+import net.minecraft.block.Block;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.DimensionManager;
+
+public class CommandExit  extends CommandBase{
+
+	private final List aliases;
+  
+	public CommandExit(){
+		aliases = new ArrayList(); 
+        aliases.add("exit"); 
+	}
+	
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return "exit";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender p_71518_1_) {
+		// TODO Auto-generated method stub
+		return "/exit";
+	}
+
+	@Override
+	public List getCommandAliases() {
+		// TODO Auto-generated method stub
+		return this.aliases;
+	}
+
+	@Override
+	public void processCommand(ICommandSender sender, String[] args) {
+		EntityPlayerMP player = getCommandSenderAsPlayer(sender);
+		World world = sender.getEntityWorld();
+		
+		if (world.isRemote) 
+        { 
+            //System.out.println("Not processing on Client side"); 
+        } 
+		else
+		{
+			if(player.dimension == 8) {
+				//Remove the player from the experiment if no arguments are passed
+				ExperimentManager.INSTANCE.checkAndRemovePlayerFromExperimentLists(player.getDisplayName());
+				EntityPlayerMP playerMP = (EntityPlayerMP) player;
+				playerMP.mcServer.getConfigurationManager().transferPlayerToDimension(playerMP, 0,	new PolycraftTeleporter(playerMP.mcServer.worldServerForDimension(0)));
+			} else {
+				player.addChatMessage(new ChatComponentText("You are not currently in a running experiment"));
+			}
+		}
+		
+	}
+	
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender p_71519_1_) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public List addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] p_82358_1_, int p_82358_2_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
