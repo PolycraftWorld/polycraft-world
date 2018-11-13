c7f251069a6b470ef1daab11e21b4806658bd65e
Dhruv Narayanan <dhruv@polycraftworld.com>
2018-11-13 11:18:44 -0600

    Very Broken - Can't instantiate the Inventory

--- src/main/java/edu/utd/minecraft/mod/polycraft/inventory/textwall/TextWallBlock.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/inventory/textwall/TextWallBlock.java
@@ -5,7 +5,11 @@ import cpw.mods.fml.relauncher.SideOnly;
 import edu.utd.minecraft.mod.polycraft.config.Inventory;
 import edu.utd.minecraft.mod.polycraft.inventory.PolycraftInventoryBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 public class TextWallBlock extends PolycraftInventoryBlock<TextWallInventory> {
 
@@ -20,8 +24,14 @@ public class TextWallBlock extends PolycraftInventoryBlock<TextWallInventory> {
 	public TextWallBlock(Inventory config, Class tileEntityClass) {
 		super(config, tileEntityClass);
 		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+		
 		// TODO Auto-generated constructor stub
 	}
+	
+//	@Override
+//	public TileEntity createNewTileEntity(World world, int var1) {
+//		return (TileEntity) new TextWallBlock(config, tileEntityClass);
+//	}
 	
 
 
--- src/main/java/edu/utd/minecraft/mod/polycraft/inventory/textwall/TextWallInventory.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/inventory/textwall/TextWallInventory.java
@@ -6,15 +6,25 @@ import java.util.List;
 import com.google.common.collect.Lists;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import edu.utd.minecraft.mod.polycraft.PolycraftRegistry;
+import edu.utd.minecraft.mod.polycraft.client.gui.GuiConsent;
 import edu.utd.minecraft.mod.polycraft.config.Inventory;
 import edu.utd.minecraft.mod.polycraft.crafting.ContainerSlot;
 import edu.utd.minecraft.mod.polycraft.crafting.GuiContainerSlot;
 import edu.utd.minecraft.mod.polycraft.crafting.PolycraftContainerType;
+import edu.utd.minecraft.mod.polycraft.crafting.PolycraftCraftingContainer;
 import edu.utd.minecraft.mod.polycraft.inventory.PolycraftInventory;
 import edu.utd.minecraft.mod.polycraft.inventory.PolycraftInventoryBlock;
+import edu.utd.minecraft.mod.polycraft.inventory.PolycraftInventoryGui;
 import edu.utd.minecraft.mod.polycraft.inventory.computer.ComputerBlock;
+import edu.utd.minecraft.mod.polycraft.inventory.computer.ComputerContainer;
+import edu.utd.minecraft.mod.polycraft.inventory.computer.ComputerGui;
 import edu.utd.minecraft.mod.polycraft.inventory.computer.ComputerInventory;
+import net.minecraft.block.BlockSign;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.InventoryPlayer;
 
 public class TextWallInventory extends PolycraftInventory {
 	
@@ -24,13 +34,29 @@ public class TextWallInventory extends PolycraftInventory {
 	public static final void register(final Inventory config) {
 		TextWallInventory.config = config;
 		config.containerType = PolycraftContainerType.TEXT_WALL;
-		PolycraftInventory.register(new TextWallBlock(config, TextWallInventory.class));
+		PolycraftInventory.register(new PolycraftInventoryBlock(config, TextWallInventory.class));
 	}
 
+	//BlockSign sign = new BlockSign(null, isVanilla);
 	
 	public TextWallInventory(PolycraftContainerType containerType, Inventory config) {
-		super(containerType, config);
+		super(PolycraftContainerType.TEXT_WALL, TextWallInventory.config);
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Override
+	public PolycraftCraftingContainer getCraftingContainer(final InventoryPlayer playerInventory) {
+		//return new ComputerContainer(playerInventory, this);
+//		return new PolycraftCraftingContainerGeneric(this, playerInventory, 128);
+		return null;
+	}
+
+//	@SideOnly(Side.CLIENT)
+//	public GuiScreen getGui(final InventoryPlayer playerInventory) {
+//		return new GuiConsent();
+//		//return new ComputerGui(this, playerInventory, 459/2, 511/2);
+////		return new ComputerGui(this, playerInventory, 342/2, 330/2);
+//		//return new PolycraftInventoryGui(this, playerInventory, 200, 208, true);
+//	}
 
 }
