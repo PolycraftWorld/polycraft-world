df0e125e00d667e519da73ef73590e36c93cc272
Stephen Goss <Stephengs94@gmail.com>
2018-11-12 19:15:38 -0600

    added KBB test

--- src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemKnockbackBomb.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemKnockbackBomb.java
@@ -5,7 +5,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 import edu.utd.minecraft.mod.polycraft.PolycraftMod;
 import edu.utd.minecraft.mod.polycraft.config.CustomObject;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
+import net.minecraft.world.World;
 
 public class ItemKnockbackBomb  extends ItemCustom{
 	
@@ -16,6 +19,23 @@ public class ItemKnockbackBomb  extends ItemCustom{
 		if (config.maxStackSize > 0)
 			this.setMaxStackSize(config.maxStackSize);
 		
+	}
+	
+	@Override
+	public void onUsingTick(ItemStack stack, EntityPlayer player, int count) {
+		// TODO Auto-generated method stub
+		super.onUsingTick(stack, player, count);
+		
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer player) {
+		// TODO Auto-generated method stub
+		
+		
+		player.motionX = 1;
+		
+		return super.onItemRightClick(itemstack, world, player);
 	}
 
 }
