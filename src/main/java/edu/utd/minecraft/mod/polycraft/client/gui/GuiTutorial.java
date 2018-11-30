475ec3f63420ea33713fe3c77645f45c4cffa121
thedhruvn <thedhruvn@users.noreply.github.com>
2018-11-30 15:40:40 -0600

    Test Merge

--- src/main/java/edu/utd/minecraft/mod/polycraft/client/gui/GuiTutorial.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/client/gui/GuiTutorial.java
@@ -1,8 +1,10 @@
 package edu.utd.minecraft.mod.polycraft.client.gui;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import edu.utd.minecraft.mod.polycraft.PolycraftMod;
+import edu.utd.minecraft.mod.polycraft.util.UtilityGL11Debug;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
@@ -14,27 +16,27 @@ public class GuiTutorial extends GuiScreen {
 
 	 private ResourceLocation BACKGROUND_IMG;// = new ResourceLocation(PolycraftMod.getAssetName("textures/gui/test_large_background.png"));
 	 private EntityPlayer player;
-	 //private int guiSettings;
-	// private int defaultGuiSettings;
+	 private int guiSettings;
+	 private int defaultGuiSettings;
 	
 	public GuiTutorial(EntityPlayer player) {
 		// TODO Auto-generated constructor stub
 		this.player = player;
-		BACKGROUND_IMG = new ResourceLocation(PolycraftMod.getAssetName("textures/gui/tutorial/test1/Slide1.png"));
-		//guiSettings = 1; //set scaled resolution to 1
-		//defaultGuiSettings = this.mc.gameSettings.guiScale;
+		BACKGROUND_IMG = new ResourceLocation(PolycraftMod.getAssetName("textures/gui/tutorial/test1/Slide2.png"));
+		guiSettings = 1; //set scaled resolution to 1
+		defaultGuiSettings = 0;//this.mc.gameSettings.guiScale;
 		//default powerpoint slide size:
 		//720px width, 405px height.
 	}
 	
 	public void resetGuiScale() {
-	//	this.guiSettings = this.defaultGuiSettings;
+		this.guiSettings = this.defaultGuiSettings;
 	}
 	
 	@Override
 	public void setWorldAndResolution(Minecraft mc, int scaledWidth, int scaledHeight) {
 		
-	//	mc.gameSettings.guiScale = this.guiSettings;
+		mc.gameSettings.guiScale = this.guiSettings;
 //		int k = 1; //set the scaled resolution to the native screen resolution of the display
 //		CustomScaledResolution scaledResolution = new CustomScaledResolution(mc, mc.displayWidth, mc.displayHeight, k);
 //		
@@ -56,7 +58,7 @@ public class GuiTutorial extends GuiScreen {
 	
 	@Override
 	public void initGui() {
-	//	this.defaultGuiSettings = 0;
+		this.defaultGuiSettings = 0;
 		super.initGui();
 		
 		
@@ -67,8 +69,8 @@ public class GuiTutorial extends GuiScreen {
 	
 	@Override
 	public void onGuiClosed() {
-		//this.resetGuiScale();
-		//this.mc.gameSettings.guiScale = this.guiSettings;
+		this.resetGuiScale();
+		this.mc.gameSettings.guiScale = this.guiSettings;
 		super.onGuiClosed();
 		//mc.gameSettings.guiScale = 0;
 		
@@ -96,11 +98,20 @@ public class GuiTutorial extends GuiScreen {
         int cur_width = this.width;
         int cur_height = this.height;
         
-        float scaleX = cur_width/width;
-        float scaleY = cur_height/height;
+        float scaleX = (float)cur_width/width;
+        float scaleY = (float)cur_height/height;
+        
+       // UtilityGL11Debug.dumpAllIsEnabled();
+        
+       
         
         GL11.glPushMatrix();
-        GL11.glScalef(scaleX, scaleY, 1);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+        GL11.glScalef(1/scaleX, 1/scaleY, 1);
+        //GL11.GL_TEXTURE_WRAP_S = GL11.GL_CLAMP;
+        //GL11.glPushClientAttrib(GL11.GL_CLAMP);
+       // 
         //GL11.glEnable(GL11.GL_BLEND);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         //GL11.glS
@@ -111,11 +122,18 @@ public class GuiTutorial extends GuiScreen {
         //int j = (this.height - 184) / 2; //old was 200
        // this.drawTexturedModalRect(i, j, 0, 0, 248, screenContainerHeight + 30);
         
-        int x_start = Math.round((float)(width*x_border));
-        int y_start = Math.round((float)(height*y_offset));
-        
-        this.drawTexturedModalRect(10, 10, 0, 0, width, 405);
+//        int x_start = Math.round((float)(width*x_border));
+//        int y_start = Math.round((float)(height*y_offset));
+       // UtilityGL11Debug.dumpAllIsEnabled();
+        //GL11.glScalef(scaleX, scaleY, 1);
+        //this.drawTexturedModalRect(10, 10, 0, 25, 720, 405);
+       this.drawTexturedModalRect(10, 10, 0, 0, (int) (720 * cur_width / width * 2), (int) (405 * cur_width / width * 2));
+        //this.drawTexturedModalRect(10, 10, 0, 25, (int) (cur_width*scaleX*.9), (int) (cur_height*scaleY*.9));
         GL11.glPopMatrix();
+        GL11.glPushMatrix();
+        GL11.glPopMatrix();
+        
+       // GL11.glPopClientAttrib();
         
     }
     
