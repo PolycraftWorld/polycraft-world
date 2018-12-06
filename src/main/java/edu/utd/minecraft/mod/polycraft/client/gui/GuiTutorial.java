e4e3346e1bb756fe4db36fd5f73cfb0aea19221e
thedhruvn <thedhruvn@users.noreply.github.com>
2018-12-06 11:52:24 -0600

    Images can be downloaded from any URL and displayed in-game
    Game handles auto-resizing, too!
    TODO:
    	- Get URL List from Server
    	- Add Next/Back buttons if more than one image exists (i.e. series of
    images)
    	- Save image in a local file as opposed to a generic location (so that
    it only has to be downloaded once)

--- src/main/java/edu/utd/minecraft/mod/polycraft/client/gui/GuiTutorial.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/client/gui/GuiTutorial.java
@@ -1,11 +1,16 @@
 package edu.utd.minecraft.mod.polycraft.client.gui;
 
+import java.io.File;
+
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 import edu.utd.minecraft.mod.polycraft.PolycraftMod;
+import edu.utd.minecraft.mod.polycraft.util.ThreadDownloadGUIImage;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.texture.ITextureObject;
+import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
@@ -14,14 +19,19 @@ import net.minecraftforge.common.MinecraftForge;
 public class GuiTutorial extends GuiScreen {
 
 	 private ResourceLocation BACKGROUND_IMG;// = new ResourceLocation(PolycraftMod.getAssetName("textures/gui/test_large_background.png"));
+	 private ResourceLocation OTHER_RESOURCE;
 	 private EntityPlayer player;
+	 private ThreadDownloadGUIImage downloadedImage;
 	 //private int guiSettings;
 	// private int defaultGuiSettings;
 	
 	public GuiTutorial(EntityPlayer player) {
 		// TODO Auto-generated constructor stub
 		this.player = player;
+		String url = "https://upload.wikimedia.org/wikipedia/commons/c/c6/Sierpinski_square.jpg";
 		BACKGROUND_IMG = new ResourceLocation(PolycraftMod.getAssetName("textures/gui/tutorial/Slide/Slide1.png"));
+		OTHER_RESOURCE = new ResourceLocation(PolycraftMod.getAssetName("textures/gui/tutorial/temp/test1.png"));
+		downloadedImage = new ThreadDownloadGUIImage(url, null, OTHER_RESOURCE);
 		//guiSettings = 1; //set scaled resolution to 1
 		//defaultGuiSettings = this.mc.gameSettings.guiScale;
 		//default powerpoint slide size:
@@ -117,7 +127,9 @@ public class GuiTutorial extends GuiScreen {
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         //GL11.glS
-        this.mc.getTextureManager().bindTexture(BACKGROUND_IMG);
+        //this.mc.getTextureManager().bindTexture(BACKGROUND_IMG);
+        this.mc.getTextureManager().loadTexture(OTHER_RESOURCE, downloadedImage);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, ((ITextureObject)downloadedImage).getGlTextureId());
         //PolycraftMod.logger.debug("Screen width & Height: " + this.width + " " + this.height);
         //System.out.println("Screen width & Height: " + this.width + " " + this.height);
         //int i = (this.width - 248) / 2;
