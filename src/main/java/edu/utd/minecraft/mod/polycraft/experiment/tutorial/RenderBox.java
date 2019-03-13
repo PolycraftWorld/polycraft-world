771c69cc3e29ebd270e3bcae3428246782829f2f
Stephen Goss <Stephengs94@gmail.com>
2019-03-13 17:17:42 -0500

    Can select points of interest or areas to make configs for tutorial maps

--- src/main/java/edu/utd/minecraft/mod/polycraft/experiment/tutorial/RenderBox.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/experiment/tutorial/RenderBox.java
@@ -11,6 +11,7 @@ import com.google.gson.Gson;
 
 import edu.utd.minecraft.mod.polycraft.privateproperty.ServerEnforcer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.Vec3;
 
 public class RenderBox {
 
@@ -22,13 +23,16 @@ public class RenderBox {
 	private int maxWarnTicks;
 	private Color color = Color.RED;
 	private float lineWidth = 8;
+	private int maxBreathingTicks = 0; //Breathing effect cycle time (0 for no effect)
+	private int breathingTicks = 0;
+	private int breathingDir = 1; //1 for fading in , -1 for fading out
 	private double x1, z1, x2, z2, y, h, xRange, zRange, range, xRange2, zRange2, range2;
 
 	public RenderBox(double x1, double z1, double x2, double z2, double y, double h, int warnTicks) {
 		this.x1 = Math.min(x1, x2);
 		this.z1 = Math.min(z1, z2);
-		this.x2 = Math.max(x1, x2);
-		this.z2 = Math.max(z1, z2);
+		this.x2 = Math.max(x1 +1, x2+1);
+		this.z2 = Math.max(z1 +1, z2 +1);
 		init(y, h, warnTicks);
 	}
 
@@ -60,6 +64,8 @@ public class RenderBox {
 			this.range2 = this.range * 2;
 		this.xRange2 = xRange / 2;
 		this.zRange2 = zRange / 2;
+		this.maxBreathingTicks = 160;
+		
 	}
 
 	public void render(Entity entity) {
@@ -83,53 +89,45 @@ public class RenderBox {
 		double z2o2 = z2 - zRange2;
 		double y1 = y + 0.01;
 		double y11 = y + 0.02;
-		double y2 = y + entity.posY + Y_TOP;
+		//double y2 = y + entity.posY + Y_TOP;
+		double y2 = y + h + 0.02;
 		float horizWidth = lineWidth;
 		float vertWidth = lineWidth;
 		if (allDist > 0)
 			horizWidth *= ((range2 - allDist) / range2);
 		if (horizDist > 0)
 			vertWidth *= ((range2 - horizDist) / range2);
-
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glDisable(GL11.GL_CULL_FACE);
-		GL11.glDisable(GL11.GL_LIGHTING);
-		GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, 0.2F);
-		GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
-
-		GL11.glVertex3d(x1, y1, z1);
-		GL11.glVertex3d(x2, y1, z1);
-		GL11.glVertex3d(x1, y1, z2);
-		GL11.glVertex3d(x2, y1, z2);
-
-		GL11.glEnd();
-
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glDisable(GL11.GL_CULL_FACE);
-		GL11.glDisable(GL11.GL_LIGHTING);
-		GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, 0.2F);
-		GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
-
-		GL11.glVertex3d(x1o1, y11, z1o1);
-		GL11.glVertex3d(x2o1, y11, z1o1);
-		GL11.glVertex3d(x1o1, y11, z2o1);
-		GL11.glVertex3d(x2o1, y11, z2o1);
-
-		GL11.glEnd();
+		
+		float alphaLines, alphaFace;
+		if(maxBreathingTicks == 0) {
+			alphaFace = 0.2F;
+			alphaLines = 0.5F;
+		}else {
+			float alphaMult = (float) ((breathingTicks * 1.0)/maxBreathingTicks);
+			alphaFace = 0.2F * alphaMult + 0.1F;
+			alphaLines = 0.5F * alphaMult  + 0.15F;
+			if(breathingTicks == maxBreathingTicks) {
+				breathingDir = -1;
+			}else if(breathingTicks == 0) {
+				breathingDir = 1;
+			}
+			breathingTicks += breathingDir;
+		}
+		
+		
+		
+		
 
 		if (horizWidth > 0) {
 			GL11.glDisable(GL11.GL_TEXTURE_2D);
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			GL11.glDisable(GL11.GL_LIGHTING);
-			GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, 0.5F);
+			GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, alphaLines);
 			GL11.glLineWidth(horizWidth);
 			GL11.glBegin(GL11.GL_LINES);
-
+			
+			//bottom box lines
 			GL11.glVertex3d(x1, y1, z1);
 			GL11.glVertex3d(x2, y1, z1);
 			GL11.glVertex3d(x2, y1, z1);
@@ -138,6 +136,16 @@ public class RenderBox {
 			GL11.glVertex3d(x1, y1, z2);
 			GL11.glVertex3d(x1, y1, z2);
 			GL11.glVertex3d(x1, y1, z1);
+			
+			//top box lines
+			GL11.glVertex3d(x1, y2, z1);
+			GL11.glVertex3d(x2, y2, z1);
+			GL11.glVertex3d(x2, y2, z1);
+			GL11.glVertex3d(x2, y2, z2);
+			GL11.glVertex3d(x2, y2, z2);
+			GL11.glVertex3d(x1, y2, z2);
+			GL11.glVertex3d(x1, y2, z2);
+			GL11.glVertex3d(x1, y2, z1);
 
 			GL11.glEnd();
 		}
@@ -147,7 +155,7 @@ public class RenderBox {
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			GL11.glDisable(GL11.GL_LIGHTING);
-			GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, 0.5F);
+			GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, alphaLines);
 			GL11.glLineWidth(vertWidth);
 			GL11.glBegin(GL11.GL_LINES);
 
@@ -162,11 +170,49 @@ public class RenderBox {
 
 			GL11.glEnd();
 		}
+		
+		Vec3 v1, v2, v3, v4;
+
+		//bottom box face
+		v1 = Vec3.createVectorHelper(x1, y1, z1);
+		v2 = Vec3.createVectorHelper(x2, y1, z1);
+		v3 = Vec3.createVectorHelper(x1, y1, z2);
+		v4 = Vec3.createVectorHelper(x2, y1, z2);
+		drawFace(v1, v2, v3, v4, alphaFace);
+		
+		//top box face
+		v1 = Vec3.createVectorHelper(x1, y2, z1);
+		v2 = Vec3.createVectorHelper(x2, y2, z1);
+		v3 = Vec3.createVectorHelper(x1, y2, z2);
+		v4 = Vec3.createVectorHelper(x2, y2, z2);
+		drawFace(v1, v2, v3, v4, alphaFace);
 
 		// GL11.glEnable(GL11.GL_CULL_FACE);
 		// GL11.glEnable(GL11.GL_LIGHTING);
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glDisable(GL11.GL_BLEND);
+	}
+	
+	private void drawFace(Vec3 v1, Vec3 v2, Vec3 v3, Vec3 v4, float alpha) {
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glDisable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glColor4f(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, alpha);
+		GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
+
+		//top box face
+		GL11.glVertex3d(v1.xCoord, v1.yCoord, v1.zCoord);
+		GL11.glVertex3d(v2.xCoord, v2.yCoord, v2.zCoord);
+		GL11.glVertex3d(v3.xCoord, v3.yCoord, v3.zCoord);
+		GL11.glVertex3d(v4.xCoord, v4.yCoord, v4.zCoord);
+
+		GL11.glEnd();
+	}
+	
+	private void drawFace(Vec3 v1, Vec3 v2, Vec3 v3, Vec3 v4) {
+		drawFace(v1, v2, v3, v4, 0.2F);
 	}
 
 	public static boolean clientRender = false;
--- src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemDevTool.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemDevTool.java
@@ -1,5 +1,6 @@
 package edu.utd.minecraft.mod.polycraft.item;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -12,6 +13,7 @@ import edu.utd.minecraft.mod.polycraft.entity.EntityOilSlimeBallProjectile;
 import edu.utd.minecraft.mod.polycraft.entity.boss.AttackWarning;
 import edu.utd.minecraft.mod.polycraft.experiment.tutorial.RenderBox;
 import edu.utd.minecraft.mod.polycraft.inventory.territoryflag.TerritoryFlagBlock;
+import edu.utd.minecraft.mod.polycraft.scoreboards.Team.ColorEnum;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.creativetab.CreativeTabs;
@@ -22,6 +24,7 @@ import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
@@ -30,10 +33,29 @@ public class ItemDevTool extends ItemCustom  {
 	
 	int[] pos1= new int[3];
 	int[] pos2 = new int[3];
+	ArrayList<Vec3> POIs = new ArrayList<Vec3>();
 	boolean pos1set = false;
 	boolean pos2set = false;
 	String tool;
 	boolean setting;
+	private StateEnum currentState;
+	public static enum StateEnum {
+		AreaSelection,
+		POISelection,
+		GuideTool;
+		
+		public StateEnum next() {
+		    if (ordinal() == values().length - 1)
+		    	return values()[0];
+		    return values()[ordinal() + 1];
+		}
+		
+		public StateEnum previous() {
+		    if (ordinal() == 0)
+		    	return values()[values().length - 1];
+		    return values()[ordinal() - 1];
+		}
+	}
 	public static ArrayList renderboxes= new ArrayList();
 	
 	
@@ -44,7 +66,7 @@ public class ItemDevTool extends ItemCustom  {
 		if (config.maxStackSize > 0)
 			this.setMaxStackSize(config.maxStackSize);
 		this.setting =false;
-	
+		currentState = StateEnum.AreaSelection;
 	}
 		
 	@Override
@@ -62,39 +84,98 @@ public class ItemDevTool extends ItemCustom  {
 	}
 
 	
+	@Override
+	public ItemStack onItemRightClick(ItemStack p_77659_1_, World world, EntityPlayer player) {
+		if(!world.isRemote)
+			return super.onItemRightClick(p_77659_1_, world, player);		
+		if(player.isSneaking()) {
+			currentState = currentState.next();
+			player.addChatMessage(new ChatComponentText(currentState.toString() + "Mode"));
+		}
+		return super.onItemRightClick(p_77659_1_, world, player);
+	}
+	
 	
 	@Override
 	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
 			float hitX, float hitY, float hitZ) {
-		player.addChatMessage(new ChatComponentText("It worked right!  " + x + "::" + y + "::" + z));
-		pos2[0] = x;
-		pos2[1] = y;
-		pos2[2] = z;
-		pos2set = true;
-		if(player.worldObj.isRemote)
-			updateRenderBoxes();
+		if(!world.isRemote) {
+			return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
+		}
+		if(!player.isSneaking()) {		
+			switch(currentState) {
+				case AreaSelection:
+					player.addChatMessage(new ChatComponentText("pos2 selected: " + x + "::" + y + "::" + z));
+					pos2[0] = x;
+					pos2[1] = y;
+					pos2[2] = z;
+					pos2set = true;
+					if(player.worldObj.isRemote)
+						updateRenderBoxes();
+					break;
+				case POISelection:
+					boolean removed = false;
+					for(int i =0;i<POIs.size();i++) {
+						if(POIs.get(i).xCoord == x && POIs.get(i).yCoord == y && POIs.get(i).zCoord == z) {
+							POIs.remove(i);
+							player.addChatMessage(new ChatComponentText("removed POI at: " + x + "::" + y + "::" + z));
+							updateRenderBoxes();
+							removed = true;
+						}
+					}
+					if(!removed) {
+						POIs.add(Vec3.createVectorHelper(x, y, z));
+						player.addChatMessage(new ChatComponentText("Added POI at: " + x + "::" + y + "::" + z));
+						updateRenderBoxes();
+					}
+					break;
+				default:
+					break;
+			}
+		}
 
 		return super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
 	}
 	
 	@Override
 	public boolean onBlockStartBreak(ItemStack itemstack, int X, int Y, int Z, EntityPlayer player) {
-		// TODO Auto-generated method stub
-		player.addChatMessage(new ChatComponentText("It worked left!  " + X + "::" + Y + "::" + Z));
-		player.worldObj.scheduleBlockUpdate(X, Y, Z, player.worldObj.getBlock(X, Y, Z), 20);
-		pos1[0] = X;
-		pos1[1] = Y;
-		pos1[2] = Z;
-		pos1set = true;
-		if(player.worldObj.isRemote)
-			updateRenderBoxes();
+		
+		if(!player.worldObj.isRemote) {
+			return true;
+		}
+		
+		switch(currentState) {
+			case AreaSelection:
+				player.addChatMessage(new ChatComponentText("pos1 selected: " + X + "::" + Y + "::" + Z));
+				player.worldObj.scheduleBlockUpdate(X, Y, Z, player.worldObj.getBlock(X, Y, Z), 20);
+				pos1[0] = X;
+				pos1[1] = Y;
+				pos1[2] = Z;
+				pos1set = true;
+				if(player.worldObj.isRemote)
+					updateRenderBoxes();
+				break;
+			case POISelection:
+				break;
+			default:
+				break;
+		}
+		
 		return true;
 	}
 	
 	private void updateRenderBoxes() {
 		renderboxes.clear();
+		
+		if(!POIs.isEmpty()) {
+			for(Vec3 v: POIs) {
+				RenderBox box = new RenderBox(v.xCoord, v.zCoord, v.xCoord, v.zCoord, v.yCoord, 1, 1);
+				box.setColor(Color.GREEN);
+				renderboxes.add(box);
+			}
+		}
 		if(pos1[0] != 0 || pos1[1] != 0 ||pos1[2] != 0 || pos2[0] != 0 || pos2[1] != 0 ||pos2[2] != 0) {
-			renderboxes.add(new RenderBox(pos1[0], pos1[2], pos2[0], pos2[2], pos1[1], 1, 2));
+			renderboxes.add(new RenderBox(pos1[0], pos1[2], pos2[0], pos2[2], Math.min(pos1[1], pos2[1]), Math.abs(pos1[1]-pos2[1]) + 1, 1));
 		}
 	}
 	
