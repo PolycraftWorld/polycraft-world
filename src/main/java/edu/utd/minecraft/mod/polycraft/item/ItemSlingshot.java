8709d6dda192a1797453841a66eb30d0eb75a263
DRZ170000 <DRZ170000@MSE83576.campus.ad.utdallas.edu>
2019-03-29 15:59:57 -0500

    Disabled KBB and FKB in half time.

--- src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemSlingshot.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemSlingshot.java
@@ -1,13 +1,16 @@
+
 package edu.utd.minecraft.mod.polycraft.item;
 
 import edu.utd.minecraft.mod.polycraft.config.CustomObject;
+import edu.utd.minecraft.mod.polycraft.item.ItemSlingshot__Old.SlingshotType;
 
-public class ItemSlingshot extends ItemCustom{
-	
+public class ItemSlingshot extends ItemCustom {
+	  
 	public enum SlingshotType{
 		WOODEN, TACTICAL, SCATTER, BURST, GRAVITY, ICE;
 	}
-
+    private int numTicksSinceLastShot = 0;
+	SlingshotType type;
 	public ItemSlingshot(CustomObject config) {
 		super(config);
 	}
