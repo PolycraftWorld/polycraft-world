ca8c4328987935530c967966cf37ddaacfd5c00f
Venkatesh Gotimukul <venkateshganu@gmail.com>
2019-06-10 09:36:21 -0500

    Industrial Oven recipes are working.

--- src/main/java/edu/utd/minecraft/mod/polycraft/crafting/RecipeGenerator.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/crafting/RecipeGenerator.java
@@ -374,6 +374,19 @@ public class RecipeGenerator {
 					ImmutableMap.of('x', craftingItemStack), 0);
 		}
 
+		for (final Object furnaceRecipeEntry : FurnaceRecipes.instance().getSmeltingList().entrySet()) {
+			final Map.Entry<ItemStack, ItemStack> furnaceRecipe = (Map.Entry<ItemStack, ItemStack>) furnaceRecipeEntry;
+			try {
+				PolycraftMod.recipeManagerRuntime.addShapelessRecipe(
+						PolycraftContainerType.INDUSTRIAL_OVEN,
+						furnaceRecipe.getValue(),
+						ImmutableList.of(furnaceRecipe.getKey()),
+						MathHelper.getRandomIntegerInRange(rand, 3, 7));
+			} catch (final Exception e) {
+				System.err.println("Unable to generate industrial oven recipe: " + furnaceRecipe.getKey().getDisplayName() + " => " + furnaceRecipe.getValue().getDisplayName());
+				System.err.println(e.getMessage());
+			}
+		}
 		/*
 		for (final GrippedTool grippedTool : GrippedTool.registry.values())
 			PolycraftMod.recipeManagerRuntime.addShapelessRecipe(
@@ -398,41 +411,6 @@ public class RecipeGenerator {
 								ImmutableList.of(PolycraftRegistry.getItemStack(s,1), grippedSyntheticTool.grip.getItemStack(PolycraftMod.recipeGripsPerTool)));
 
 			}
-		}
-		
-		for (final PogoStick pogoStick : PogoStick.registry.values())
-			if (pogoStick.source != null && pogoStick.grip != null)
-				PolycraftMod.recipeManagerRuntime.addShapelessRecipe(
-						PolycraftContainerType.CRAFTING_TABLE,
-						pogoStick.getItemStack(),
-						ImmutableList.of(pogoStick.source.getItemStack(), pogoStick.grip.getItemStack(PolycraftMod.recipeGripsPerPogoStick)));
-
-		for (final Armor armor : Armor.registry.values()) {
-			final ItemStack craftingItemStack = PolycraftRegistry.getItemStack(armor.craftingItemName, 1);
-			PolycraftMod.recipeManagerRuntime.addShapedRecipe(
-					coloringFactory,
-					PolycraftContainerType.CRAFTING_TABLE,
-					ImmutableList.of(PolycraftRegistry.getItemStack(armor.getFullComponentName(ArmorSlot.HEAD), 1)),
-					new String[] { "xxx", "x x", "   " },
-					ImmutableMap.of('x', craftingItemStack), 0);
-			PolycraftMod.recipeManagerRuntime.addShapedRecipe(
-					coloringFactory,
-					PolycraftContainerType.CRAFTING_TABLE,
-					ImmutableList.of(PolycraftRegistry.getItemStack(armor.getFullComponentName(ArmorSlot.CHEST), 1)),
-					new String[] { "x x", "xxx", "xxx" },
-					ImmutableMap.of('x', craftingItemStack), 0);
-			PolycraftMod.recipeManagerRuntime.addShapedRecipe(
-					coloringFactory,
-					PolycraftContainerType.CRAFTING_TABLE,
-					ImmutableList.of(PolycraftRegistry.getItemStack(armor.getFullComponentName(ArmorSlot.LEGS), 1)),
-					new String[] { "xxx", "x x", "x x" },
-					ImmutableMap.of('x', craftingItemStack), 0);
-			PolycraftMod.recipeManagerRuntime.addShapedRecipe(
-					coloringFactory,
-					PolycraftContainerType.CRAFTING_TABLE,
-					ImmutableList.of(PolycraftRegistry.getItemStack(armor.getFullComponentName(ArmorSlot.FEET), 1)),
-					new String[] { "   ", "x x", "x x" },
-					ImmutableMap.of('x', craftingItemStack), 0);
 		}
 
 		for (final Tool tool : Tool.registry.values()) {
@@ -480,21 +458,6 @@ public class RecipeGenerator {
 					ImmutableList.of(PolycraftRegistry.getItemStack(tool.getFullTypeName(Tool.Type.AXE), 1)),
 					new String[] { "xx ", "xy ", " y " },
 					ImmutableMap.of('x', craftingHeadItemStack, 'y', craftingShaftItemStack), 0);
-		}
-
-		//add all furnace recipes to the industrial oven
-		for (final Object furnaceRecipeEntry : FurnaceRecipes.instance().getSmeltingList().entrySet()) {
-			final Map.Entry<ItemStack, ItemStack> furnaceRecipe = (Map.Entry<ItemStack, ItemStack>) furnaceRecipeEntry;
-			try {
-				PolycraftMod.recipeManagerRuntime.addShapelessRecipe(
-						PolycraftContainerType.INDUSTRIAL_OVEN,
-						furnaceRecipe.getValue(),
-						ImmutableList.of(furnaceRecipe.getKey()),
-						MathHelper.getRandomIntegerInRange(rand, 3, 7));
-			} catch (final Exception e) {
-				System.err.println("Unable to generate industrial oven recipe: " + furnaceRecipe.getKey().getDisplayName() + " => " + furnaceRecipe.getValue().getDisplayName());
-				System.err.println(e.getMessage());
-			}
 		}
 		*/
 	}
