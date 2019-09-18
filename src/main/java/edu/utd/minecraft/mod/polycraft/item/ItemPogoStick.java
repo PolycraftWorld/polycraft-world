6fbcd210eb06adf1a8611346058d1af966d399e6
Stephen Goss <Stephengs94@gmail.com>
2019-09-18 17:13:06 -0500

    Revert "PogoStick now works with AI API"
    This reverts commit 117b94960cca4a026bcae9f8a3f610c14b725d01.

--- src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemPogoStick.java
+++ src/main/java/edu/utd/minecraft/mod/polycraft/item/ItemPogoStick.java
@@ -2,26 +2,13 @@ package edu.utd.minecraft.mod.polycraft.item;
 
 import java.util.Random;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockButton;
-import net.minecraft.block.BlockContainer;
-import net.minecraft.block.BlockDoor;
-import net.minecraft.block.BlockWorkbench;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.settings.GameSettings;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 import edu.utd.minecraft.mod.polycraft.PolycraftMod;
 import edu.utd.minecraft.mod.polycraft.config.PogoStick;
-import edu.utd.minecraft.mod.polycraft.proxy.ClientProxy;
 
 public class ItemPogoStick extends PolycraftUtilityItem {
-	
-	private int pogoStickPreviousContinuousActiveBounces = 0;
-	private float pogoStickLastFallDistance = 0;
 
 	public static boolean isEquipped(final EntityPlayer player) {
 		return PolycraftItemHelper.checkCurrentEquippedItem(player, ItemPogoStick.class, true);
@@ -49,49 +36,5 @@ public class ItemPogoStick extends PolycraftUtilityItem {
 		this.setMaxDamage(config.maxBounces);
 		this.setMaxStackSize(1);
 		this.config = config;
-	}
-	
-	@Override
-	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer player) {
-//		float jumpMovementFactor = ClientProxy.baseJumpMovementFactor;
-//			jumpMovementFactor *= config.jumpMovementFactorBuff;
-//			if (!pogoStick.config.restrictJumpToGround || player.onGround) {
-		final boolean playerActivelyBouncing = noScreenOverlay() && !isPlayerLookingAtPogoCancellingBlock();
-		final boolean playerActivelySupressing = !playerActivelyBouncing && GameSettings.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindSneak) && noScreenOverlay();
-		if(player.onGround) {
-			player.motionY = config.getMotionY(playerActivelySupressing ? 0 : pogoStickLastFallDistance, pogoStickPreviousContinuousActiveBounces, playerActivelyBouncing)+0.1;
-		}
-//		final double motionY = config.getMotionY(playerActivelySupressing ? 0 : pogoStickLastFallDistance, pogoStickPreviousContinuousActiveBounces, playerActivelyBouncing);
-//		if (motionY > 0)
-//			player.motionY = motionY;
-//				if (playerActivelyBouncing)
-//					pogoStickPreviousContinuousActiveBounces++;
-//				else
-//					pogoStickPreviousContinuousActiveBounces = 0;
-//			}
-////		} else {
-////			playerState.pogoStickPreviousContinuousActiveBounces = 0;
-////		}
-//		playerState.pogoStickLastFallDistance = 0;
-//
-//		if (player.jumpMovementFactor != jumpMovementFactor)
-//			player.jumpMovementFactor = jumpMovementFactor;
-		return super.onItemRightClick(itemStackIn, worldIn, player);
-	}
-	
-	private boolean isPlayerLookingAtPogoCancellingBlock() {
-		if (Minecraft.getMinecraft().objectMouseOver != null) {
-			final Block block = Minecraft.getMinecraft().theWorld.getBlockState(Minecraft.getMinecraft().objectMouseOver.getBlockPos()).getBlock();
-			return block instanceof BlockContainer
-					|| block instanceof BlockWorkbench
-					|| block instanceof BlockDoor
-					|| block instanceof BlockButton
-					|| block == Blocks.bed;
-		}
-		return false;
-	}
-
-	private boolean noScreenOverlay() {
-		return Minecraft.getMinecraft().currentScreen == null;
 	}
 }
