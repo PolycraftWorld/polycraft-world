c0babcb4ad264f3537ec388b0d0d3cc1b885cf2c
Stephen Goss <Stephengs94@gmail.com>
2019-09-30 18:19:08 -0500

    Add directory for python API files

---
+++ PolycraftAIGym/PolycraftGym.py
@@ -0,0 +1,33 @@
+import os, subprocess, socket, json
+from pathlib import Path
+
+class PolycraftGym(object):
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def start_client(self, scene):
+        # this function is currently broken!
+        path = Path(os.getcwd())
+        # print(str(path.parent) + '\\gradlew RunClient')
+        # os.system('\"' + str(path.parent) + '\\gradlew RunClient' + '\"')
+        # subprocess.run([str(path.parent) + '\\gradlew', 'setupDecompWorkspace'])
+
+    def setup_scene(self, scene):
+        pass  # why can't I write an empty function???
+
+    def send_command(self, command):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        sock.send(command)
+        sock.close()
+
+    def step_command(self, command):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        sock.send(command)
+        data = sock.recv(10240).decode()
+        data_dict = json.loads(data)
+        print(data_dict)
+        sock.close()
