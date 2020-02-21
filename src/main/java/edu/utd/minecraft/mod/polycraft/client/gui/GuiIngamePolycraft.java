e0d6231764b659464802da3bd3a4c864294f645a
Stephen Goss <Stephengs94@gmail.com>
2020-02-21 02:00:47 -0600

    Custom GUI renderer For Future Use

---
+++ src/main/java/edu/utd/minecraft/mod/polycraft/client/gui/GuiIngamePolycraft.java
@@ -0,0 +1,17 @@
+package edu.utd.minecraft.mod.polycraft.client.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.GuiIngameForge;
+
+public class GuiIngamePolycraft extends GuiIngameForge {
+
+	public GuiIngamePolycraft(Minecraft mc) {
+		super(mc);
+	}
+
+	@Override
+	public void renderGameOverlay(float partialTicks) {
+		
+		super.renderGameOverlay(partialTicks);
+	}
+}
