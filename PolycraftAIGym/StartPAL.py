e8bf745bd8fe7569a1c39fccdfa59be4b5bbba71
Stephen Goss <stephengs94@gmail.com>
2020-04-30 21:21:40 -0500

    Add script for starting Polycraft

---
+++ PolycraftAIGym/StartPAL.py
@@ -0,0 +1,48 @@
+import subprocess
+import sys
+
+
+def execute(command):
+    process = subprocess.Popen(command, shell=True, cwd='../', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    count = 0
+    # Poll process for new output until finished
+    while True:
+        nextline = process.stdout.readline()
+        if nextline == '' and process.poll() is not None:
+            break
+        if len(str(nextline)) > 3:
+            sys.stdout.write(str(nextline) + '\n')
+            sys.stdout.flush()
+        # count += 1
+        # if count > 25:
+        #     break
+
+    output = process.communicate()[0]
+    exitCode = process.returncode
+
+    if (exitCode == 0):
+        return output
+    else:
+        raise subprocess.CalledProcessError(exitCode, command)
+    # popen = subprocess.Popen(cmd, cwd='../', stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+    # for stdout_line in iter(popen.stdout.readline, ""):
+    #     yield stdout_line
+    # popen.stdout.close()
+    # return_code = popen.wait()
+    # if return_code:
+    #     raise subprocess.CalledProcessError(return_code, cmd)
+
+
+def main():
+    #run polycraft client
+    print("test")
+    #execute("cd ..")
+    print("test")
+    execute("call gradlew runclient")
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
+
+
